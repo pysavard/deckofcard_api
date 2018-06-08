b7b29ceecf85bd939d3e8ade9e008debf8d92a6e
FACILITE\psavard <psavard@facilite.com>
2018-06-08 02:26:25 -0400

    fix tests

--- src/test/java/com/py/deckofcard/restapi/deck/bll/DeckServiceImplementationTest.java
+++ src/test/java/com/py/deckofcard/restapi/deck/bll/DeckServiceImplementationTest.java
@@ -1,67 +1,86 @@
 package com.py.deckofcard.restapi.deck.bll;
 
 import com.py.deckofcard.restapi.TestBase;
+import com.py.deckofcard.restapi.deck.dao.DeckCollectionDao;
 import com.py.deckofcard.restapi.deck.dto.CardDto;
 import com.py.deckofcard.restapi.deck.entity.Card;
+import com.py.deckofcard.restapi.deck.entity.Deck;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 public class DeckServiceImplementationTest extends TestBase {
-/*
+
     @Mock
-    private DeckDao deckDao;
+    private DeckCollectionDao deckDao;
 
     @InjectMocks
     private DeckServiceImplementation sut;
 
     @Test
-    public void shuffle_shouldCreateEmptyDeckOfCards()
-    {
-        sut.shuffle();
+    public void shuffle_shouldEmptyDeckBeforeCreateNew() {
+        Deck deck = mock(Deck.class);
+        when(deckDao.getDeck(1)).thenReturn(deck);
 
-        verify(deckDao).emptyDeck();
+        sut.shuffle(1);
+
+        verify(deck).clearDeck();
     }
 
     @Test
-    public void shuffle_shouldCreateDeckWith52Card(){
-        sut.shuffle();
+    public void shuffle_shouldCreateADeckOf52Cards() {
+        Deck deck = mock(Deck.class);
+        when(deckDao.getDeck(1)).thenReturn(deck);
 
-        verify(deckDao, times(52)).addCard(any(), any());
+        sut.shuffle(1);
+
+        verify(deck, times(52)).addCard(any(), any());
     }
 
     @Test
-    public void dealOneCard_whenDeckNotEmpty_shouldReturnRandomCard(){
+    public void dealOneCard_whenDeckNotEmpty_shouldReturnRandomCard() {
         Card card1 = fixture.create(Card.class);
         Card card2 = fixture.create(Card.class);
-        when(deckDao.getAllCard()).thenReturn(Arrays.asList(card1, card2));
+        ArrayList<Card> cards = new ArrayList<>();
+        cards.add(card1);
+        cards.add(card2);
 
-        CardDto result  = sut.dealOneCard();
+        Deck deck = mock(Deck.class);
+        when(deckDao.getDeck(1)).thenReturn(deck);
+
+        when(deck.getCards()).thenReturn(cards);
+
+        CardDto result = sut.dealOneCard(1);
 
         assertThat(card1.equalTo(result) || card2.equalTo(result)).isTrue();
+
     }
 
     @Test
-    public void dealOneCard_whenDeckNotEmpty_shouldRemoveDealtCard(){
+    public void dealOneCard_whenDeckNotEmpty_shouldRemoveDealtCard() {
         Card card = fixture.create(Card.class);
-        when(deckDao.getAllCard()).thenReturn(Arrays.asList(card));
+        Deck deck = new Deck(1);
+        when(deckDao.getDeck(1)).thenReturn(deck);
 
-        sut.dealOneCard();
+        deck.addCard(card);
 
-        verify(deckDao).removeCard(card);
+        CardDto result = sut.dealOneCard(1);
+
+        assertThat(deck.getCards()).isEmpty();
     }
 
     @Test
-    public void dealOneCard_whenDeckEmpty_shouldReturnEmptyCard(){
+    public void dealOneCard_whenDeckEmpty_shouldReturnEmptyCard() {
         CardDto emptyCard = new CardDto();
-
-        CardDto result = sut.dealOneCard();
+        Deck deck = new Deck(1);
+        when(deckDao.getDeck(1)).thenReturn(deck);
+        CardDto result = sut.dealOneCard(1);
 
         assertThat(result).isEqualToComparingFieldByField(emptyCard);
-    }*/
+    }
 }
